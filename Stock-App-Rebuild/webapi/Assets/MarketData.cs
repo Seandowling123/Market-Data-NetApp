1ffdeecba23753e3366ffb7c6685ebfde0e9ddc9
Seandowling123 <dowling.sean103@gmail.com>
2023-10-17 13:49:55 +0100

    initial commit

---
+++ Stock-App-Rebuild/webapi/Assets/MarketData.cs
@@ -0,0 +1,24 @@
+﻿namespace Stonk_App_BackEnd.Assets
+{
+    public class MarketData
+    {
+        public DateTime Date { get; set; }
+        public string? DateString { get; set; }
+        public double OpenPrice { get; set; }
+        public double HighPrice { get; set; }
+        public double LowPrice { get; set; }
+        public double ClosePrice { get; set; }
+        public double Volume { get; set; }
+
+        public override string ToString()
+        {
+            return
+            $"Date : {DateString}{Environment.NewLine}" +
+            $"Open : {OpenPrice}{Environment.NewLine}" +
+            $"High : {HighPrice}{Environment.NewLine}" +
+            $"Low : {LowPrice}{Environment.NewLine}" +
+            $"Close : {ClosePrice}{Environment.NewLine}" +
+            $"Volume : {Volume}{Environment.NewLine}";
+        }
+    }
+}
---
+++ Stock-App-Rebuild/webapi/DataReceiver.cs
@@ -0,0 +1,66 @@
+﻿using Stonk_App_BackEnd.Assets;
+
+namespace Stonk_App_BackEnd
+{
+    class Receiver
+    {
+        public List<MarketData> PrintData(string symbol, string interval)
+        {
+            string API_key = @"ZXOVMBOFQ9MKYRYA";
+            string QUERY_URL;
+
+            if (interval == "Past Year")
+            {
+                QUERY_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=" + symbol + "&apikey=" + API_key + "&datatype=csv";
+            }
+            else if (interval == "Past Month")
+            {
+                QUERY_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + symbol + "&apikey=" + API_key + "&datatype=csv";
+            }
+            else
+            {
+                QUERY_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=15min" + "&apikey=" + API_key + "&datatype=csv";
+            }
+
+            Uri queryUri = new Uri(QUERY_URL);
+
+            List<MarketData> data = new List<MarketData>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = client.GetStreamAsync(queryUri).Result;
+
+                using (StreamReader sr = new StreamReader(response))
+                {
+                    String[] fields = new string[5];
+                    string line;
+                    int i = 0;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        i = 0;
+                        fields = line.Split(',');
+
+                        if (fields[0] == "timestamp") { continue; }
+
+                        data.Add(new MarketData()
+                        {
+                            DateString = fields[i],
+                            Date = DateTime.Parse(fields[i++]),
+                            OpenPrice = double.Parse(fields[i++]),
+                            HighPrice = double.Parse(fields[i++]),
+                            LowPrice = double.Parse(fields[i++]),
+                            ClosePrice = double.Parse(fields[i++]),
+                            Volume = double.Parse(fields[i++])
+                        });
+                    }
+                    /*foreach (interday aPart in data)
+                    {
+                        Console.WriteLine(aPart);
+                    }*/
+                }
+            }
+            return data;
+        }
+    }
+}
